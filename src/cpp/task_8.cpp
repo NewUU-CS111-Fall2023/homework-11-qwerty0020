0e75f742b7b646e5a84701352ca6e2864e64eaa7
qwerty0020 <127468946+qwerty0020@users.noreply.github.com>
2024-01-23 14:16:00 +0500

    Update task_8.cpp

--- src/cpp/task_8.cpp
+++ src/cpp/task_8.cpp
@@ -1,9 +1,53 @@
 /*
- * Author:
- * Date:
- * Name:
+ * Author: Muxammadjon Axmadjonov
+ * Date: 23/01/2024
+ * Name: hw-11
  */
 
-class Problem8 {
-public:
-};
+#include <iostream>
+#include <algorithm>
+
+
+int gcd(int a, int b) {
+    while (b) {
+        a %= b;
+        std::swap(a, b);
+    }
+    return a;
+}
+
+int modular_inverse(int a, int m) {
+    int m0 = m;
+    int y = 0, x = 1;
+    while (a > 1) {
+        int q = a / m;
+        int t = m;
+        m = a % m;
+        a = t;
+        t = x;
+        x = y - q * x;
+        y = t;
+    }
+    if (x < 0) x += m0;
+    return x;
+}
+
+int main() {
+    int p = 19;
+    int q = 43;
+    int n = p * q;
+    int phi_n = (p - 1) * (q - 1);
+
+
+    int e = 2;
+    while (gcd(e, phi_n) != 1) {
+        e++;
+    }
+
+
+    int d = modular_inverse(e, phi_n);
+
+    std::cout << "Public exponent (e): " << e << std::endl;
+    std::cout << "Private exponent (d): " << d << std::endl;
+    return 0;
+}
