ea47625e628cfbd1516ab20b8c353cdf6e51a4a0
qwerty0020 <127468946+qwerty0020@users.noreply.github.com>
2024-01-23 14:09:36 +0500

    Update task_6.cpp

--- src/cpp/task_6.cpp
+++ src/cpp/task_6.cpp
@@ -1,9 +1,40 @@
 /*
- * Author:
- * Date:
- * Name:
+ * Author: Muxammadjon Axmadjonov
+ * Date: 23/01/2024
+ * Name: hw-11
  */
 
-class Problem6 {
-public:
-};
+#include <iostream>
+
+using namespace std;
+
+bool is_prime(int num) {
+    if (num <= 1) {
+        return false;
+    }
+    for (int i = 2; i * i <= num; i++) {
+        if (num % i == 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+void generate_primes(int n) {
+    int count = 0;
+    int num = n + 1;
+    while (count < 2) {
+        if (is_prime(num)) {
+            cout << num << " ";
+            count++;
+        }
+        num++;
+    }
+    cout << endl;
+}
+
+int main() {
+    int n = 10;
+    generate_primes(n);
+    return 0;
+}
