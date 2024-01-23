f08139de08edcf3dd44a48107331edd15171a59e
qwerty0020 <127468946+qwerty0020@users.noreply.github.com>
2024-01-23 14:12:51 +0500

    Update task_7.cpp

--- src/cpp/task_7.cpp
+++ src/cpp/task_7.cpp
@@ -1,9 +1,24 @@
 /*
- * Author:
- * Date:
- * Name:
+ * Author: Muxammadjon Axmadjonov
+ * Date: 23/01/2024
+ * Name: hw-11
  */
 
-class Problem7 {
-public:
-};
+#include <iostream>
+
+int phi(int n) {
+    if (n == 1) return 0;
+    for (int i = 2; i * i <= n; ++i) {
+        if (n % i == 0) return phi(n / i) * (i - 1);
+    }
+    return n - 1;
+}
+
+int main() {
+    int a = 13;
+    int b = 17;
+    int n = a * b;
+    int phi_n = phi(n);
+    std::cout << "phi(" << n << ") = " << phi_n << std::endl;
+    return 0;
+}
