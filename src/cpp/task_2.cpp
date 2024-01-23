5660c8e66527f4b6588c1e2bb477fc74abc8f914
qwerty0020 <127468946+qwerty0020@users.noreply.github.com>
2024-01-23 10:40:55 +0500

    Update task_2.cpp

--- src/cpp/task_2.cpp
+++ src/cpp/task_2.cpp
@@ -1,9 +1,41 @@
 /*
- * Author:
- * Date:
- * Name:
+ * Author: Muxammadjon Axmadjonov
+ * Date: 23/01/2024
+ * Name: hw-11
  */
 
-class Problem2 {
-public:
-};
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+bool can_partition(vector<int>& nums) {
+  int total_sum = 0;
+  for (int num : nums) {
+    total_sum += num;
+  }
+
+  if (total_sum % 2 != 0) {
+    // The sum is odd, so it cannot be partitioned into two equal subsets.
+    return false;
+  }
+
+  int target_sum = total_sum / 2;
+  vector<bool> dp(target_sum + 1, false);
+  dp[0] = true;
+
+  for (int num : nums) {
+    for (int j = target_sum; j >= num; j--) {
+      dp[j] = dp[j] || dp[j - num];
+    }
+  }
+
+  return dp[target_sum];
+}
+
+int main() {
+  vector<int> nums = {1, 5, 11, 5};
+  bool can_partition = can_partition(nums);
+  cout << "Can partition? " << can_partition << endl;
+  return 0;
+}
